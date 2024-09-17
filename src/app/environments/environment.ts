942431e9a7137de395d29fee082879071809ef0e
Cassius Hill <MarcelusWork@gmail.com>
2024-09-16 22:15:06 -0600

    finished timesheet project

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDPsUGukmQtiPLQU13B1BTVdhLMB8OYoBI",
+        authDomain: "timesheet-1e19b.firebaseapp.com",
+        projectId: "timesheet-1e19b",
+        storageBucket: "timesheet-1e19b.appspot.com",
+        messagingSenderId: "696891312670",
+        appId: "1:696891312670:web:73cbd511d310f985ea7c0f",
+        measurementId: "G-DTVCSNH1J1"
+      }
+  };
